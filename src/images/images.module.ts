aeb76ee00eea13d194bedb8b3390bbfdb4769ed7
marekchoinski <marekchoinski@mac-1.home>
2025-12-01 15:50:00 +0100

    Add image module

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ImagesController } from './images.controller';
+import { ImagesService } from './images.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [ImagesController],
+  providers: [ImagesService],
 })
 export class ImagesModule {}
 
