6e8aee194af79040ec5e649993d4e5debfd93726
marekchoinski <marekchoinski@mac-1.home>
2025-12-01 18:15:00 +0100

    Add muller

---
+++ src/config/multer.config.ts
@@ -0,0 +1,14 @@
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+export const multerConfig = {
+  storage: diskStorage({
+    destination: './uploads',
+    filename: (req, file, callback) => {
+      const uniqueName = `${uuidv4()}${extname(file.originalname)}`;
+      callback(null, uniqueName);
+    },
+  }),
+};
+
