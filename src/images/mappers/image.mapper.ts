079fa1f997f2f34b2c72fac2be0afedeb0223c15
marekchoinski <marekchoinski@mac-1.home>
2025-12-01 17:40:00 +0100

    Fix image

--- src/images/mappers/image.mapper.ts
+++ src/images/mappers/image.mapper.ts
@@ -1,5 +1,15 @@
-import { Image } from '@prisma/client';
 import { ImageResponseDto } from '../dto/image-response.dto';
+
+interface Image {
+  id: string;
+  title: string;
+  path: string;
+  url: string;
+  width: number;
+  height: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export function toImageResponseDto(image: Image): ImageResponseDto {
   return {
